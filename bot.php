77022d4b6399df7d8eb6f8f3da9ed34e70fa861d
KanyuKim <67403134+KanyuKim@users.noreply.github.com>
2020-08-07 14:58:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,426 +1,76 @@
 <?php
-#-------------------------[Include]-------------------------#
-require_once('./include/line_class.php');
-require_once('./unirest-php-master/src/Unirest.php');
-#-------------------------[Token]-------------------------#
-$channelAccessToken = 'OT1Mr8FCNG/GDY8oxIVa7H++ZmXL5hwltJy9BA/P8bfY46vn/Cu8gnRCT2qwafiYwQA672DCMBmEJEtLyGBWdCBILQmXKKSCx32Mo6yey93gvkZBCKWDYj2X0YnjVNepLslfspYsbKbvTU/g3+NXUgdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = '0c384592a626e8ff8076b0dbd34aa5b9';
-#-------------------------[Events]-------------------------#
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-$userId     = $client->parseEvents()[0]['source']['userId'];
-$groupId    = $client->parseEvents()[0]['source']['groupId'];
-$replyToken = $client->parseEvents()[0]['replyToken'];
-$timestamp  = $client->parseEvents()[0]['timestamp'];
-$type       = $client->parseEvents()[0]['type'];
-$message    = $client->parseEvents()[0]['message'];
-$profile    = $client->profil($userId);
-$repro = json_encode($profile);
-$messageid  = $client->parseEvents()[0]['message']['id'];
-$msg_type      = $client->parseEvents()[0]['message']['type'];
-
-$post_data      = $client->parseEvents()[0]['postback']['data'];
-
-
-
-$msg_file      = $client->parseEvents()[0]['message']['fileName'];
-$msg_message   = $client->parseEvents()[0]['message']['text'];
-$msg_title     = $client->parseEvents()[0]['message']['title'];
-$msg_address   = $client->parseEvents()[0]['message']['address'];
-$msg_latitude  = $client->parseEvents()[0]['message']['latitude'];
-$msg_longitude = $client->parseEvents()[0]['message']['longitude'];
-#----Check title empty----#
-if (empty($msg_title)) {
-    $msg_title = 'ที่ไหนก็ได้ โตแล้ว';
+// กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+// include composer autoload
+require_once '../vendor/autoload.php';
+ 
+// การตั้งเกี่ยวกับ bot
+require_once 'bot_settings.php';
+ 
+// กรณีมีการเชื่อมต่อกับฐานข้อมูล
+//require_once("dbconnect.php");
+ 
+///////////// ส่วนของการเรียกใช้งาน class ผ่าน namespace
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+//use LINE\LINEBot\Event;
+//use LINE\LINEBot\Event\BaseEvent;
+//use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\MessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use LINE\LINEBot\ImagemapActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder ;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
+ 
+// เชื่อมต่อกับ LINE Messaging API
+$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+ 
+// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
+$content = file_get_contents('php://input');
+ 
+// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+$events = json_decode($content, true);
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
 }
-#----command option----#
-$usertext = explode(" ", $message['text']);
-$command = $usertext[0];
-$options = $usertext[1];
-if (count($usertext) > 2) {
-    for ($i = 2; $i < count($usertext); $i++) {
-        $options .= '+';
-        $options .= $explode[$i];
-    }
+// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+$textMessageBuilder = new TextMessageBuilder(json_encode($events));
+ 
+//l ส่วนของคำสั่งตอบกลับข้อความ
+$response = $bot->replyMessage($replyToken,$textMessageBuilder);
+if ($response->isSucceeded()) {
+    echo 'Succeeded!';
+    return;
 }
-#----command option----#
-$remsg = json_encode($message, true);
-$remsg1 = json_decode($remsg, true);
-$remsg2 = $remsg1['text'];
-$stickerId = $remsg1['stickerId'];
-$reline = json_encode($profile, true);
-$reline1 = json_decode($reline, true);
-$reline2 = $reline1['displayName'];
-
-
-
-
-
-#-------------------------[EVENT TYPE]-------------------------#
-if ($type == 'memberJoined') {
-    $text = "เมื่อมีผู้ใช้เข้ากลุ่ม";
-        $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            )
-        )
-    );
-}
-elseif ($type == 'memberLeft') {
-    $text = "เมื่อมีผู้ใช้ออกกลุ่ม";
-        $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            )
-        )
-    );
-}
-elseif ($type == 'join') {
-      $text = "เมื่อบอทถูกเชิญเข้าห้อง";
-    $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            )
-        )
-    );
-}
-elseif ($type == 'leave') {
-    $text = "เมื่อบอทถูกเตะออกจากห้อง";
-        $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            )
-        )
-    );
-}
-elseif ($type == 'follow') {
-    $text = "เมื่อผู้ใช้กดติดตามบอท";
-    $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            )
-        )
-    );
-}
-elseif ($type == 'unfollow') {
-    $text = "เมื่อบอทถูกบล็อค";
-        $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            )
-        )
-    );
-}
-#-------------------------[MSG TYPE]-------------------------#
-elseif ($msg_type == 'file') {
-$url = 'https://api.line.me/v2/bot/message/' . $messageid . '/content';
-$headers = array('Authorization: Bearer ' . $channelAccessToken);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-$ran = date("YmdHis");
-$botDataUserFolder = './user/file/file/' . $userId;
-                    if(!file_exists($botDataUserFolder)) {
-                        mkdir($botDataUserFolder, 0777, true);
-                    } 
-$fileFullSavePath = $botDataUserFolder . '/' . $ran . $msg_file;
-$fileurl = 'https://ktbatmbot.herokuapp.com' . $fileFullSavePath;
-file_put_contents($fileFullSavePath,$result);
-  $text = "บันทึกไฟล์เรียบร้อยแล้ว";
-      $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            ),
-            array(
-                'type' => 'text',
-                'text' => $fileurl
-            )
-        )
-    );
-}
-
-elseif ($msg_type == 'image') {
-$url = 'https://api.line.me/v2/bot/message/' . $messageid . '/content';
-$headers = array('Authorization: Bearer ' . $channelAccessToken);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-$ran = date("YmdHis");
-$botDataUserFolder = './user/file/image/' . $userId;
-                    if(!file_exists($botDataUserFolder)) {
-                        mkdir($botDataUserFolder, 0777, true);
-                    } 
-$fileFullSavePath = $botDataUserFolder . '/' . $ran . '.jpg';
-$picurl = 'https://ktbatmbot.herokuapp.com' . $fileFullSavePath;
-file_put_contents($fileFullSavePath,$result);
-  $text = "บันทึกไฟล์รูปภาพเรียบร้อยแล้ว";
-      $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            ),
-            array(
-                'type' => 'text',
-                'text' => $picurl
-            )
-        )
-    );
-}
-elseif ($msg_type == 'video') {
-  $url = 'https://api.line.me/v2/bot/message/' . $messageid . '/content';
-$headers = array('Authorization: Bearer ' . $channelAccessToken);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-$ran = date("YmdHis");
-$botDataUserFolder = './user/file/video/' . $userId;
-                    if(!file_exists($botDataUserFolder)) {
-                        mkdir($botDataUserFolder, 0777, true);
-                    } 
-$fileFullSavePath = $botDataUserFolder . '/' . $ran . '.mp4';
-$vidurl = 'https://ktbatmbot.herokuapp.com' . $fileFullSavePath;
-file_put_contents($fileFullSavePath,$result);
-  $text = "บันทึกไฟล์วิดีโอเรียบร้อยแล้ว";
-      $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            ),
-            array(
-                'type' => 'text',
-                'text' => $vidurl
-            )
-        )
-    );
-}
-elseif ($msg_type == 'audio') {
-  $url = 'https://api.line.me/v2/bot/message/' . $messageid . '/content';
-$headers = array('Authorization: Bearer ' . $channelAccessToken);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-$ran = date("YmdHis");
-$botDataUserFolder = './user/file/audio/' . $userId;
-                    if(!file_exists($botDataUserFolder)) {
-                        mkdir($botDataUserFolder, 0777, true);
-                    } 
-$fileFullSavePath = $botDataUserFolder . '/' . $ran . '.m4a';
-$audurl = 'https://ktbatmbot.herokuapp.com' . $fileFullSavePath;
-file_put_contents($fileFullSavePath,$result);
-  $text = "บันทึกไฟล์เสียงเรียบร้อยแล้ว";
-      $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            ),
-            array(
-                'type' => 'text',
-                'text' => $audurl
-            )
-        )
-    );
-}
-elseif ($msg_type == 'sticker') {
-  $stickerurl = "https://stickershop.line-scdn.net/stickershop/v1/sticker/" . $stickerId . "/android/sticker.png";
-      $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-          
-array(
-        'type' => 'flex',
-        'altText' => 'Sticker!!',
-        'contents' => array(
-        'type' => 'bubble',
-        'body' => array(
-          'type' => 'box',
-          'layout' => 'vertical',
-          'spacing' => 'md',
-          'contents' => array(
-            array(
-              'type' => 'text',
-          'align' => 'center',
-          'color' => '#049b1b',
-          'text' => 'USER : ' . $reline2
-      ),
-            array(
-          'type' => 'image',
-          'size' => '5xl',
-          'align' => 'center',
-          'url' => $stickerurl
-      )
-        )
-        )
-        )
-        )
-    )
-    );
-}
-elseif ($msg_type == 'location') {
-
-    $uri = "https://api.openweathermap.org/data/2.5/weather?lat=" . $msg_latitude . "&lon=" . $msg_longitude . "&lang=th&units=metric&appid=bb32ab343bb6e3326f9e1bbd4e4f5d31";
-    $response = Unirest\Request::get("$uri");
-    $json = json_decode($response->raw_body, true);
-    $resulta = $json['name'];
-    $resultb = $json['weather'][0]['main'];
-    $resultc = $json['weather'][0]['description'];
-    $resultd = $json['main']['temp'];
-    $resulte = $json['coord']['lon'];
-
-    $text .= " พื้นที่ : " . $resulta . "\n";
-    $text .= " สภาพอากาศ : " . $resultb . "\n";
-    $text .= " รายละเอียด : " . $resultc . "\n";
-    $text .= " อุณหภูมิ : " . $resultd;
-
-      $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'location',
-                'title' => $msg_title,
-                'address' => $msg_address,
-                'latitude' => $msg_latitude,
-                'longitude' => $msg_longitude
-            ),            array(
-                'type' => 'text',
-                'text' => $text
-            )
-        )
-    );
-
-}
-
-else { 
-
-if ($command== 'myid') { 
-
-  $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => 'userId ของคุณคือ '.$userId,
-'quickReply' => array(
-    'items' => array(
-     array(
-      'type' => 'action',
-      'action' => array(
-       'type' => 'postback',
-       'label' => 'Postback',
-       'data' => 'happy'
-      )
-     )
-    )
-   )
-
-            )
-        )
-    );
-}
-
-elseif ($command== 'qr' || $command== 'Qr' || $command== 'QR' || $command== 'Qrcode' || $command== 'QRcode' || $command== 'qrcode') { 
-      $url = 'https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=300x300&chl='.$options;
-      $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'image',
-                'originalContentUrl' => $url,
-                'previewImageUrl' => $url
-            )
-        )
-    );
-}
-
-elseif ($post_data== 'happy') { 
-
-  $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => 'Postback : happy',
-'quickReply' => array(
-    'items' => array(
-     array(
-      'type' => 'action',
-      'action' => array(
-       'type' => 'postback',
-       'label' => 'Postback',
-       'data' => 'happy'
-      )
-     )
-    )
-   )
-
-            )
-        )
-    );
-}
-
-
-else {
-    
-    
-     $text = "ข้อความอื่นๆ";
-      $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-            ),
-            array(
-                'type' => 'text',
-                'text' => $fileurl
-            )
-        )
-    );
-           
-}
-if (isset($mreply)) {
-    $result = json_encode($mreply);
-    $client->replyMessage($mreply);
-}  
-    file_put_contents('log.txt',file_get_contents('php://input'));
+ 
+// Failed
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
-
-    
-
